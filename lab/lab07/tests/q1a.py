ae9ffbefb7646ff1fd718d4fb23afdf765477e46
Rubina Aujla <raujla@berkeley.edu>
2020-07-13 09:43:28 -0700

    lab 7 release

---
+++ lab/lab07/tests/q1a.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q1a',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> np.isclose(xbar, 19.78594262295082)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(ybar, 2.9982786885245898)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(stdx, 8.8841505777711323)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(stdy, 1.3807999538298954)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> type(r) == np.float64\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(r, 0.67573410921136412)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
