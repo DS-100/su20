c59a66951f059c63bd4ca8a861b70b060e63c260
Kevin Moy <kevinmoy@berkeley.edu>
2020-07-20 13:59:09 -0700

    Minor updates to lab 9

--- lab/lab09/tests/q4.py
+++ lab/lab09/tests/q4.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q4',
     'points': 1,
-    'suites': [   {   'cases': [   {   'code': ">>> np.isclose(compute_CV_error(lm.LinearRegression(), X_train[['TAX', 'INDUS', 'CRIM']],Y_train), 7.761704927406191)\nFalse",
+    'suites': [   {   'cases': [   {   'code': ">>> np.isclose(compute_CV_error(lm.LinearRegression(), X_train2[['TAX', 'INDUS', 'CRIM']],Y_train2), 7.761704927406191)\nTrue",
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
