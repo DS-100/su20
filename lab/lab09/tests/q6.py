576789d0b22b32bc81c34ddea175f0630f9352df
Kevin Moy <kevinmoy@berkeley.edu>
2020-07-20 19:17:53 -0700

    fix lab 9 q6 check

--- lab/lab09/tests/q6.py
+++ lab/lab09/tests/q6.py
@@ -1,7 +1,1 @@
-test = {   'name': 'q6',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> (np.abs(test_rmse - 5.197762151588737) < 1e-3) or (np.abs(test_rmse - 4.635857923518503) < 1e-3)\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
+test = {'name': 'q6', 'points': 1, 'suites': [{'cases': [{'code': '>>> test_rmse <= 5.2\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
