c0651619014ae676c22c8a3f92bbf4858ec206a6
Rubina Aujla <raujla@berkeley.edu>
2020-07-22 09:13:25 -0700

    lab10 release

---
+++ lab/lab10/tests/q3.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q3',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> np.isclose(lr_loss(np.array([0, 0]), X_new, Y), 0.69314718055994529)\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> np.isclose(lr_loss(theta,\n'
+                                               '...                           X_new,\n'
+                                               '...                           Y),\n'
+                                               '...                   0.29120889606613115)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
