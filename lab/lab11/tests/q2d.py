6079902c2eee6952600cb973d351074fa95be0a3
Shichao Han <schan21@berkeley.edu>
2020-07-28 12:38:17 +0800

    add lab11

---
+++ lab/lab11/tests/q2d.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2d',
+    'points': 1,
+    'suites': [{'cases': [{'code': ">>> random_forest_model.criterion == 'gini'\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
