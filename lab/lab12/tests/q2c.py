b5a62559b351826b74470fde6c89fd011d6aed61
Kevin Moy <kevinmoy@berkeley.edu>
2020-07-28 22:00:03 -0700

    Lab 12 released

---
+++ lab/lab12/tests/q2c.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2c',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> len(boot_sample_std) == m\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> 7 < boot_mean_std < 10\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> 0.23 < boot_var_std < 0.29\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
